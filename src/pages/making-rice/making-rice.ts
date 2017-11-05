40f114369aa4e26695babb6ee98f49c643935bd4
adamhe17 <shuaihe2018@u.northwestern.edu>
2017-11-05 01:21:06 -0600

    added estimate timer while cooking

--- src/pages/making-rice/making-rice.ts
+++ src/pages/making-rice/making-rice.ts
@@ -9,15 +9,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MakingRicePage {
 
   dots: number;
+  remainingTime: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.dots = 0;
+    this.remainingTime = 35;
   }
 
   ionViewDidLoad() {
     setInterval(() => {
       this.dots = (this.dots + 1) % 4;
     }, 600);
+
+    var r = setInterval(() => {
+      this.remainingTime--;
+      if (this.remainingTime <= 0) {
+        clearInterval(r);
+      }
+    }, 60000);
   }
 
 }
